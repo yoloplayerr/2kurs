6f712bcb4ce075e4a3b5b396dbbc7ad450eb70c6
Maxim <yoloplayerr@users.noreply.github.com>
2019-09-25 10:09:33 +0300

    someNewCommit

--- SecondCourse/src/lab1/Lab1.java
+++ SecondCourse/src/lab1/Lab1.java
@@ -1,41 +1,54 @@
 package lab1;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Random;
 
 import lab1.Function;
 
 public class Lab1 {
-
+	private static NumberFormat formatter = new DecimalFormat("#0.0000");
 	public static void main(String[] args) {
-		/*
-		 * Лист функций
-		 */
+		
 		ArrayList<Function> funcList = new ArrayList<>();
+		
 		/*
-		 * Функция синуса
+		 * Задания 1-3
 		 */
 		Function lambdaSin = (x) -> 2 * Math.sin(x) + 1;
-		funcList.add(lambdaSin);
-//		System.out.println(getFunc(lambdaSin));
-		System.out.println();
-		/*
-		 * Квадратичная функция
-		 */
 		Function lambdaQuad1 = (x) -> Math.pow(x / Math.PI - 1, 2);
-		funcList.add(lambdaQuad1);
-//		System.out.println(getFunc(lambdaQuad1));
-		/*
-		 * Квадратичная функция #2
-		 */
 		Function lambdaQuad2 = (x) -> (-Math.pow(x / Math.PI, 2) - 2 * x + 5 * Math.PI);
-		funcList.add(lambdaQuad2);
-//		System.out.println(getFunc(lambdaQuad2));
-		/*
-		 * Функция косинуса
-		 */
 		Function lambdaCos = (x) -> ((0.5) * (Math.pow((Math.cos(x)), 2)) + 1);
+		
+		funcList.add(lambdaSin);
+		funcList.add(lambdaQuad1);
+		funcList.add(lambdaQuad2);
 		funcList.add(lambdaCos);
-//		System.out.println(getFunc(lambdaCos));
+		
+		/*
+		 * Вывод значений функции
+		 */
+		System.out.println(getFunc(lambdaSin));
+		System.out.println(getFunc(lambdaQuad1));
+		System.out.println(getFunc(lambdaQuad2));
+		System.out.println(getFunc(lambdaCos));
+		
+		/*
+		 *Максимум минимум 
+		 */
+		getMinMaxValue(lambdaSin);				
+		getMinMaxValue(lambdaQuad1);
+		getMinMaxValue(lambdaQuad2);
+		getMinMaxValue(lambdaCos);
+		
+		
+		Function someFunc=(x)->x*Math.sin(x)-0.5;
+		System.out.println(getFuncRes(Math.PI, 0, someFunc));
+		
+		
+		
+
 		int count=0;	
 		for (int i = 0; i < funcList.size(); i++) {
 			count=0;
@@ -44,20 +57,47 @@ public class Lab1 {
 						count++;
 					}
 				}
-				System.out.println(getFunc(funcList.get(i)));
-				System.out.println("Количество отрицательных значений:"+count);
+//				System.out.println(getFunc(funcList.get(i)));
+//				System.out.println("Количество отрицательных значений:"+count);
 				
 				
 		}
 		
 
 	}
-	public static double getMinMaxValue() {
-		double[] x=new double[20];
-			for(int i=0;i<x.length;i++) {
-				x[i]=-10+i;				
+	public static void getMinMaxValue(Function lambda) {
+		int n=10;
+		Random random = new Random();	
+			int[] array=new int[n];
+		double tempMin=100000000000000.0;
+		double tempMax=-tempMin;	
+				for(int i=0;i<array.length;i++) {
+					array[i]=random.nextInt(11+10) -10;			
+				}
+				for(int i=0;i<array.length;i++) {
+					if(lambda.funcValue(array[i])>tempMax){
+						tempMax=lambda.funcValue(array[i]);
+					}else if(lambda.funcValue(array[i])<tempMin) {
+						tempMin=lambda.funcValue(array[i]);
+					}
+				}
+				System.out.print("Максимальное значение функции :"+formatter.format(tempMax));
+				System.out.println(", Минимальное значение функции :"+formatter.format(tempMin));
+	}
+	public static double getFuncRes(double rightBord,double leftBord,Function lambda) {
+		double epsilon=0.0001;
+		double c=0;
+			while((rightBord-leftBord)>epsilon) {
+				c=(rightBord+leftBord)/2;
+					if(lambda.funcValue(c)==0) {
+						return c;
+					}else if(lambda.funcValue(c)*lambda.funcValue(leftBord)<0) {
+						rightBord=c;						
+					}else {
+						leftBord=c;
+					}						
 			}
-			return 1.0;
+			return c;
 	}
 
 	public static ArrayList<Double> getFunc(Function lambda) {
